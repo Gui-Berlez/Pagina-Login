39d0428bf5bbf747419e979366c0eb7791bc5d69
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2024-09-17 17:22:41 -0300

    commit login template page

---
+++ modelo-pagina-login/projeto-modelo-login/src/js/index.js
@@ -0,0 +1,9 @@
+const botao = document.querySelector(".login")
+const preencher = document.querySelectorAll(".preencher")
+
+
+botao.addEventListener("click", ()=>{
+alert("Você acessou sua conta!")
+    //continuar código fazendo com que só apareça o alerta caso estejam preenchidos os inputs de email e senha. Caso aperte o botão de login, sem os campos esterem preenchidos, mostre um alerta pedindo para preencher até estar preenchido conforme pedido. 
+    // A classe dos inputs é preencher
+})
