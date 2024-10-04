6c9d35ff2eeffb2b2aac1fe3730da31990b6bc72
Guilherme Jacob Berlez <guilhermeberlez@gmail.com>
2024-10-04 12:15:34 -0300

    Commit - Página 1 finalizada
    Neste commit foi feito o código em JS para o preenchimento obrigatório do input "Usuário" e do input "Senha".

--- modelo-pagina-login/projeto-modelo-login/src/js/index.js
+++ modelo-pagina-login/projeto-modelo-login/src/js/index.js
@@ -1,9 +1,34 @@
-const botao = document.querySelector(".login")
-const preencher = document.querySelectorAll(".preencher")//Ao invés do por .preencher nos parenteses, mudar para .inpubox
+//const botao = document.querySelector(".login")
+//const preencher = document.querySelectorAll(".preencher")//Ao invés do por .preencher nos parenteses, mudar para .inpubox
 
 
-botao.addEventListener("click", ()=>{
+//botao.addEventListener("click", ()=>{
+//alert("Você acessou sua conta!")
+    
+//})
+//Acima era um teste 
+
+
+document.getElementById("formulario").addEventListener("submit", function(event){
+    const usuarioInput = document.getElementById("usuario");
+    const senhaInput = document.getElementById("senha");
+
+    let mensagem = "";
+
+    if(usuarioInput.value.trim() === " "){
+        mensagem += "O campo Usuário é obrigatório\n";
+    }
+
+    if(senhaInput.value.trim() === " "){
+        mensagem += "O campo Senha é obrigatório\n";
+    }
+
+if(mensagem) {
+    event.preventDefault(); // Não deixa-rá o envio ser completado
+    alert(mensagem ); // Exibirá a mesnsagem de erro
+    usuarioInput.focus();  // Foca no campo Usuário (ou Senha)
+}
+
 alert("Você acessou sua conta!")
-    //continuar código fazendo com que só apareça o alerta caso estejam preenchidos os inputs de email e senha. Caso aperte o botão de login, sem os campos esterem preenchidos, mostre um alerta pedindo para preencher até estar preenchido conforme pedido. 
-    // A classe dos inputs é preencher
 })
+
